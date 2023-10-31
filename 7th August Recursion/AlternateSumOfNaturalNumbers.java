95a0e280ff26741508f08029245641813bbae424
AnuragDutt36 <anuragdutt3668@gmail.com>
2023-10-31 16:50:31 +0530

    Oct 31 16:50

--- 7th August Recursion/AlternateSumOfNaturalNumbers.java
+++ 7th August Recursion/AlternateSumOfNaturalNumbers.java
@@ -1,19 +1,22 @@
 public class AlternateSumOfNaturalNumbers {
     public static void main(String[] args) {
         int n1 = 10;
-        int sum1 = alternateSum(n1);
+        int sum1 = alternateSum(n1, 1);
         System.out.println("Alternate sum up to " + n1 + " is: " + sum1);
 
         int n2 = 5;
-        int sum2 = alternateSum(n2);
+        int sum2 = alternateSum(n2, 1);
         System.out.println("Alternate sum up to " + n2 + " is: " + sum2);
     }
 
-    public static int alternateSum(int n) {
-        if (n % 2 == 0) {
-            return n / 2 * (1 - n); // Sum formula for even numbers
+    public static int alternateSum(int n, int i) {
+        if (i == n + 1) {
+            return 0;
+        }
+        if (i % 2 != 0) {
+            return alternateSum(n, i + 1) + i;
         } else {
-            return (n + 1) / 2 - n / 2; // Sum formula for odd numbers
+            return alternateSum(n, i + 1) - i;
         }
     }
 }
