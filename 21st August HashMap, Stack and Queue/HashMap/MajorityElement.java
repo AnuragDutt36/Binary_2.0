9ac1d23b837e2903c705e686bcf053d665ab87ff
AnuragDutt36 <anuragdutt3668@gmail.com>
2023-11-05 15:13:18 +0530

    Nov 5 3:13PM

--- 21st August HashMap, Stack and Queue/HashMap/MajorityElement.java
+++ 21st August HashMap, Stack and Queue/HashMap/MajorityElement.java
@@ -1,22 +1,20 @@
 import java.util.*;
 
 public class MajorityElement {
-    public static void main(String[] args) {
-        int[] nums = {4, 2, 7, 1, 9};
-        System.out.println(findMajorityElement(nums)); // Output: 9
-    }
-    
     public static int findMajorityElement(int[] nums) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        int majorityThreshold = nums.length / 2;
-        
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
         for (int num : nums) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-            if (countMap.get(num) > majorityThreshold) {
-                return num;
-            }
+            map.put(num, 1);
         }
-        
-        return -1; // Majority element not found
+
+        return map.lastKey();
+
     }
+
+    public static void main(String[] args) {
+        int[] nums = { 4, 2, 7, 1, 9 };
+        System.out.println("Output: " + findMajorityElement(nums)); // Output: 9
+    }
+
 }
--- 21st August HashMap, Stack and Queue/HashMap/RansomNote.java
+++ 21st August HashMap, Stack and Queue/HashMap/RansomNote.java
@@ -2,27 +2,24 @@ import java.util.*;
 
 public class RansomNote {
     public static void main(String[] args) {
-        System.out.println(canConstruct("a", "b")); // Output: false
-        System.out.println(canConstruct("aa", "ab")); // Output: false
-        System.out.println(canConstruct("aa", "aab")); // Output: true
+        System.out.println("Output: " + canConstruct("a", "b")); // Output: false
+        System.out.println("Output: " + canConstruct("aa", "ab")); // Output: false
+        System.out.println("Output: " + canConstruct("aa", "aab")); // Output: true
     }
-    
+
     public static boolean canConstruct(String ransomNote, String magazine) {
         Map<Character, Integer> magazineMap = new HashMap<>();
-        
+
         for (char ch : magazine.toCharArray()) {
             magazineMap.put(ch, magazineMap.getOrDefault(ch, 0) + 1);
         }
 
-        System.out.println(magazineMap);
-        
         for (char ch : ransomNote.toCharArray()) {
             if (!magazineMap.containsKey(ch) || magazineMap.get(ch) <= 0) {
                 return false;
             }
             magazineMap.put(ch, magazineMap.get(ch) - 1);
         }
-        // System.out.println(magazineMap);
         return true;
     }
 }
